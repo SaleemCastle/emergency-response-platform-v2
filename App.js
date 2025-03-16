53f2f97eb4c0c0da107860d622c8f8e4665c174c
AlkalinemaxPHP <wright.jeremiah78@gmail.com>
2025-03-16 07:51:45 -0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import AppNavigator from './src/navigation/AppNavigator';
+import { Alert } from 'react-native';
+
+export default function App() {
+  return <AppNavigator />;
+}
